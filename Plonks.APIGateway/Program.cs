70e98bc6582eeda21c5006778e981cde8a4abf5f
Joris Kamminga <joris.kamminga@gmail.com>
2022-05-29 15:44:24 +0200

    fixed gateway cors origins

--- Plonks.APIGateway/Program.cs
+++ Plonks.APIGateway/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("cors", builder =>
     {
-        builder.WithOrigins("http://localhost:3000", "http://127.0.0.1:3000", "plonks.nl")
+        builder.WithOrigins("http://localhost:3000", "http://127.0.0.1:3000", "https://plonks.nl")
         .AllowAnyMethod()
         .AllowAnyHeader()
         .AllowCredentials();
