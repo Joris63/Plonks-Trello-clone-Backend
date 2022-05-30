380cdf2f80120ee9f618b48dd38db7f05ffb7915
Joris Kamminga <joris.kamminga@gmail.com>
2022-05-30 19:42:21 +0200

    Made new gateway project

--- Plonks.Gateway/Program.cs
+++ Plonks.Gateway/Program.cs
@@ -1,38 +1,6 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("cors", builder =>
-    {
-        builder.AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials();
-    });
-});
-
-var ocelotConfig = builder.Environment.IsDevelopment() ? "ocelot.Development.json" : "ocelot.json";
-builder.Configuration.AddJsonFile(ocelotConfig);
-
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOcelot();
-
 var app = builder.Build();
 
-app.UseCors("cors");
-
-app.UseHttpsRedirection();
-
-app.UseOcelot().Wait();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
