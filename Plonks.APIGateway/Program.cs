d242bb71f0b8d34d5cbccfe6f0b3c8ff601abba0
Joris Kamminga <joris.kamminga@gmail.com>
2022-05-30 18:21:01 +0200

    updated gateway

--- Plonks.APIGateway/Program.cs
+++ Plonks.APIGateway/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,14 +14,25 @@ builder.Services.AddCors(options =>
     });
 });
 
-
 var ocelotConfig = builder.Environment.IsDevelopment() ? "ocelot.Development.json" : "ocelot.json";
 builder.Configuration.AddJsonFile(ocelotConfig);
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
 app.UseCors("cors");
 
+app.UseHttpsRedirection();
+
 app.UseOcelot().Wait();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
